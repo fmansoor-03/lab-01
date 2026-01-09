90ec97cf75d90e7fd0956c6dd894958c508b38a4
fmansoor-03 <fmansoor@ualberta.ca>
2026-01-08 20:01:04 -0700

    Add constructor and getMood method to Happy class

--- PetShop/app/src/main/java/com/example/petshop/Happy.java
+++ PetShop/app/src/main/java/com/example/petshop/Happy.java
@@ -3,12 +3,16 @@ package com.example.petshop;
 import java.util.Date;
 
 public class Happy extends Mood {
+    public Happy() {
+        super();
+    }
+    
     public Happy(Date date) {
         super(date);
     }
 
     @Override
-    public void setDate(Date date) {
-        super.setDate(date);
+    public String getMood() {
+        return "happy";
     }
 }
