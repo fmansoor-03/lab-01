369560c12577d9e5cd29c20d70d3957f02b2b9ae
fmansoor-03 <fmansoor@ualberta.ca>
2026-01-08 20:01:48 -0700

    Add constructor and getMood method to Sad class

--- PetShop/app/src/main/java/com/example/petshop/Sad.java
+++ PetShop/app/src/main/java/com/example/petshop/Sad.java
@@ -3,12 +3,16 @@ package com.example.petshop;
 import java.util.Date;
 
 public class Sad extends Mood {
+    public Sad() {
+        super();
+    }
+    
     public Sad(Date date) {
         super(date);
     }
 
     @Override
-    public void setDate(Date date) {
-        super.setDate(date);
+    public String getMood() {
+        return "sad";
     }
 }
