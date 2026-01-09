d659327746a2a2db7383156813abe086d37a3c42
fmansoor-03 <fmansoor@ualberta.ca>
2026-01-08 19:59:28 -0700

    Add default constructor to Mood class

--- PetShop/app/src/main/java/com/example/petshop/Mood.java
+++ PetShop/app/src/main/java/com/example/petshop/Mood.java
@@ -4,6 +4,10 @@ import java.util.Date;
 public abstract class Mood {
     private Date date;
 
+    public Mood() {
+        this.date = new Date();
+    }
+
     public Mood(Date date) {
         this.date = date;
     }
@@ -15,4 +19,6 @@ public abstract class Mood {
     public void setDate(Date date) {
         this.date = date;
     }
+
+    public abstract String getMood();
 }
